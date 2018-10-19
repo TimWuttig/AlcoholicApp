2b89e9f9a2de30d384eaaf4129a5f062d5fc73f5
Tim Wuttig <tim@localhost.localdomain>
2018-10-19 16:10:18 +0200

    Projekt erstellt, sowie groben Aufbau des Safer Moduls

---
+++ app/src/main/java/tiwu/alcoholicapp/Safer.java
@@ -0,0 +1,57 @@
+package tiwu.alcoholicapp;
+
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.nio.Buffer;
+
+/**
+ * Created by tim on 19.10.18.
+ */
+
+public class Safer{
+    private AlcoholicApp alcoholicApp;
+    private BufferedWriter writer;
+    private BufferedReader reader;
+    private volatile boolean dirtyFlag = false; //False: Nothing changed True: Appstate must be saved
+
+    /*
+    Set connection to MainActivity and open writer and reader
+     */
+    public Safer(AlcoholicApp alcoholicApp){
+        this.alcoholicApp = alcoholicApp;
+        try {
+            writer = new BufferedWriter(new OutputStreamWriter(alcoholicApp.openFileOutput("data.txt", alcoholicApp.MODE_PRIVATE)));
+            reader = new BufferedReader(new InputStreamReader(alcoholicApp.openFileInput("data.txt")));
+
+        }catch(FileNotFoundException notFound){
+            Toast.makeText(alcoholicApp,"Error by reading App Data: Savefile was not found",Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    public synchronized void setFlag(boolean status){
+        dirtyFlag = status;
+    }
+
+    /*
+    Saves the App state
+     */
+    public void safe(){
+
+    }
+
+    /*
+    Restore the last App state
+     */
+    public void restore(){
+
+    }
+}
